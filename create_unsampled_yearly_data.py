71b8e3a96ec5eeb14f85c451961032294d925ec4
swamyhn <swamyee99@gmail.com>
2023-03-29 01:07:48 +0800

    only selected columns will be merged to yearly data

--- create_unsampled_yearly_data.py
+++ create_unsampled_yearly_data.py
@@ -4,6 +4,12 @@ import glob
 #adjustable variables
 joined_weather_path = 'path/to/joined/weather/data/'
 output_path = 'path/to/output/'
+modelling_cols = ['YEAR', 'MONTH', 'DAY_OF_MONTH', 'DAY_OF_WEEK'
+       'CRS_DEP_TIME', 'DEP_DELAY', 'DEP_DELAY_GROUP', 
+       'CRS_ARR_TIME', 'ARR_DELAY', 'ARR_DELAY_GROUP', 
+       'DISTANCE', 'PRCP_ORIGIN', 'SNOW_ORIGIN', 'SNWD_ORIGIN', 
+       'TMAX_ORIGIN', 'TMIN_ORIGIN', 'PRCP_DEST',
+       'SNOW_DEST', 'SNWD_DEST', 'TMAX_DEST', 'TMIN_DEST']
 
 all_years = set()
 for file in glob.glob(f'{joined_weather_path}airOT*.csv'):
@@ -12,7 +18,7 @@ for file in glob.glob(f'{joined_weather_path}airOT*.csv'):
 for year in all_years:
     year_dfs_list = []
     for file in glob.glob(f'{joined_weather_path}airOT{year}*.csv'):
-        year_dfs_list.append(pd.read_csv(file))
+        year_dfs_list.append(pd.read_csv(file, usecols = modelling_cols))
     year_df = pd.concat(year_dfs_list, ignore_index = True)
 
     year_df.to_csv(f'{output_path}{year}.csv', index = False)
