f9423876b7a51fffd116a90b989702feb202776c
swamyhn <swamyee99@gmail.com>
2023-03-31 00:55:04 +0800

    added modelling cols to sampling.py

--- sampling.py
+++ sampling.py
@@ -2,27 +2,33 @@ import pandas as pd
 import glob
 
 #adjustable variables
-joined_weather_path = 'path/to/joined/weather/data/'
-sampled_output_path = 'path/to/sampled/output/'
-sample_percentage = 0.1
+#joined_weather_path = 'path/to/joined/weather/data/'
+joined_weather_path = "C:/Users/swamy/OneDrive - National University of Singapore/DSA3101/Project/joined_weather/"
+#sampled_output_path = 'path/to/sampled/output/'
+sampled_output_path = "C:/Users/swamy/OneDrive - National University of Singapore/DSA3101/Project/load_db/import_csv/"
+sample_percentage = 0.2
 
-all_years = set()
-for file in glob.glob(f'{joined_weather_path}airOT*.csv'):
-    all_years.add(int(file[-10:-6]))
+modelling_cols = ['YEAR', 'MONTH', 'DAY_OF_MONTH', 'DAY_OF_WEEK',
+       'CRS_DEP_TIME', 'DEP_DELAY', 'DEP_DELAY_GROUP', 
+       'CRS_ARR_TIME', 'ARR_DELAY', 'ARR_DELAY_GROUP', 
+       'DISTANCE', 'PRCP_ORIGIN', 'SNOW_ORIGIN', 'SNWD_ORIGIN', 
+       'TMAX_ORIGIN', 'TMIN_ORIGIN', 'PRCP_DEST',
+       'SNOW_DEST', 'SNWD_DEST', 'TMAX_DEST', 'TMIN_DEST']
+
+# all_years = set()
+# for file in glob.glob(f'{joined_weather_path}airOT*.csv'):
+#     all_years.add(int(file[-10:-6]))
+
+all_years = [1989, 1990, 2000, 2001, 2006, 2007]
 
 for year in all_years:
     year_dfs_list = []
     for file in glob.glob(f'{joined_weather_path}airOT{year}*.csv'):
-        month_df = pd.read_csv(file)
-        month_df = month_df[pd.isnull(month_df['DEP_DELAY_GROUP']) == False]
-
-        #sample a certain percentage from each bin of delay
-        month_df_grouped = month_df.groupby('DEP_DELAY_GROUP')
-        sampled_month_df = month_df_grouped.apply(lambda x: x.sample(frac = sample_percentage))
-
+        month_df = pd.read_csv(file, usecols = modelling_cols)
+        sampled_month_df = month_df.sample(frac = sample_percentage)
         year_dfs_list.append(sampled_month_df)
 
     year_df = pd.concat(year_dfs_list, ignore_index = True)
 
-    year_df.to_csv(f'sampled_output_path{year}.csv', index = False)
+    year_df.to_csv(f'{sampled_output_path}{year}.csv', index = False)
     print(str(year) + ' DONE')
