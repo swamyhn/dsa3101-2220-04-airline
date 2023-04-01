0ac14d368bc1182a66aff353f29e091cea682fcc
swamyhn <swamyee99@gmail.com>
2023-04-02 02:27:57 +0800

    modified load_data.py to load null values properly

--- setup_mysqldb/load_data.py
+++ setup_mysqldb/load_data.py
@@ -20,7 +20,14 @@ cur = conn.cursor()
 # Load data into tables
 for year in years:
     file_path = f'{path_to_csv_folder}{year}.csv'
-    cur.execute(f"""LOAD DATA INFILE '{file_path}' INTO TABLE airlines.year_{year} FIELDS TERMINATED BY ',' ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;""")
+    cur.execute(f"""LOAD DATA INFILE '{file_path}' INTO TABLE airlines.year_{year} FIELDS TERMINATED BY ',' 
+    ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 ROWS 
+    (year, month, day_of_month, day_of_week, crs_dep_time, dep_delay, dep_delay_group, 
+    crs_arr_time, arr_delay, arr_delay_group, distance, prcp_origin, snow_origin, 
+    snwd_origin, tmax_origin, tmin_origin, prcp_dest, snow_dest, snwd_dest, 
+    tmax_dest, tmin_dest) SET dep_delay = NULLIF(dep_delay, ''), dep_delay_group = NULLIF(dep_delay_group, ''), 
+    crs_dep_time = NULLIF(crs_dep_time, ''), arr_delay = NULLIF(arr_delay, ''), 
+    arr_delay_group = NULLIF(arr_delay, ''), crs_arr_time = NULLIF(crs_arr_time, '');""")
     print(f'{year} LOADED')
 
 conn.commit()
