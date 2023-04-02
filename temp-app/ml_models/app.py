5ac5efea2d756c9321451150f71343aa02a7d6a9
felibunnyy <felibunbun13@gmail.com>
2023-04-03 02:09:25 +0800

    updated vis3

--- temp-app/ml_models/app.py
+++ temp-app/ml_models/app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 import main
 import joblib
 import os
@@ -10,11 +10,6 @@ app = Flask(__name__)
 def front_page():
     return "Our model outputs"
 
-# input (mode):
-# "<ml_model>_<direction>_<year>", where <ml_model>: lm, dt
-#                                  and   <direction>: arr, dep
-#                                  and   <year>: 1989, 1990, 2000, 2001, 2006, 2007
-# eg. "lm_arr_1989"
 @app.route("/coefficients", methods=['GET'])
 def get_coefficients():
 
@@ -28,7 +23,7 @@ def get_coefficients():
             myModel = joblib.load(pkl_file)
             print('---Successfully loaded pickle file---')
         else:
-            myModel = main.load_and_train(mode, ml_model)
+            myModel = main.load_and_train(mode)
             print('---Successfully loaded model---')
 
         if ml_model == 'lm':
@@ -44,3 +39,22 @@ def get_coefficients():
     except Exception as e:
         print(e)
         print("Cannot get_coefficients")
+
+@app.route("/plots")
+def return_plot():
+    try:
+        mode = request.args.get('mode')
+
+        diagram = f'{mode}.png'
+
+        if os.path.isfile(diagram):
+            print(f'---Successfully loaded {diagram}---')
+        else:
+            main.load_and_train(mode)
+            print(f'---Successfully loaded dt model and created {diagram}---')
+
+        return send_file(diagram)
+    
+    except Exception as e:
+        print(e)
+        print("Cannot render_plot")
