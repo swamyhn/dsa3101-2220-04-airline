de97be9a2f65de15159dbbcd8043c0fdaa02e131
swamyhn <swamyee99@gmail.com>
2023-04-13 15:55:29 +0800

    cleaning up frontend-temp

--- delay/json_flask_stuff.R
+++
@@ -1,7 +0,0 @@
-library(jsonlite)
-library(httr)
-
-flask_url <- "http://localhost:5000/"
-response <- GET(flask_url, path="query", query=list(origin = "ATL", dest="LAX", year=2007))
-df <- fromJSON(content(response, "text"), simplifyDataFrame = TRUE)
-View(df)
